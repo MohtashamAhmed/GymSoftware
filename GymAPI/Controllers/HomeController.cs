0382ec0f518278d37ad6e17aa586d7262bd9ab05
Mohtasham <syedshahmohtasham49@gmail.com>
2018-10-20 20:39:46 +0530

    smallchange

--- GymAPI/Controllers/HomeController.cs
+++ GymAPI/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace GymAPI.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        { 
             ViewBag.Title = "Home Page";
 
             return View();
