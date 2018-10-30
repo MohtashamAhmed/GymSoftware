da306d1386ae3887f4741b4b56373cb1baf209aa
Mohtasham <syedshahmohtasham49@gmail.com>
2018-10-30 23:32:04 +0530

    Design Changes

--- GymSoftware/Controllers/UserController.cs
+++ GymSoftware/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GymSoftware;
+using GymSoftware.Models;
 
 namespace GymSoftware.Controllers
 {
@@ -15,7 +16,18 @@ namespace GymSoftware.Controllers
         }
         public ActionResult GetAllCustomers()
         {
-            return View();
+            List<CustomerVM> ListCusVM = new List<CustomerVM>();
+           
+            for (int i = 0; i < 10; i++)
+            {
+                CustomerVM cus = new CustomerVM();
+                cus.Name = "Name" + i;
+                cus.Mobile = "8143287477";
+                cus.Source = "Social Media";
+                cus.Payment = i.ToString();
+                ListCusVM.Add(cus);
+            }
+            return View(ListCusVM);
         }
         
     }
---
+++ GymSoftware/Models/CustomerVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GymSoftware.Models
+{
+    public class CustomerVM
+    {
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Source { get; set; }
+        public string DateofBirth { get; set; }
+        public string Weight { get; set; }
+        public string JoinDate { get; set; }
+        public string TrainerID { get; set; }
+        public string MembershipID { get; set; }
+        public string BatchID { get; set; }
+        public string TotalPayment { get; set; }
+        public string Payment { get; set; }
+        public string Outstanding { get; set; }
+        public string DateOfPayment { get; set; }
+        public string ExpiryDate { get; set; }
+        public bool ExistingUser { get; set; }
+    }
+}
